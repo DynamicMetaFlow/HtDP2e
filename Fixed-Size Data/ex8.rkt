1e05b794a2f8481d6babecf1409ba3d3f67ae950
Alexander <alexander.soto@gmail.com>
2016-02-21 13:15:42 -0500

    Completed Ex.8

---
+++ Fixed-Size Data/ex8.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex8) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+#|Exercise 8. Use the picture primitives to create the image of a simple tree.|#
+
+(overlay/offset(rotate 90 (rectangle 100 40 'solid 'brown))
+                 0 -95
+                 (rotate 90 (circle 50 'solid 'darkgreen)))
