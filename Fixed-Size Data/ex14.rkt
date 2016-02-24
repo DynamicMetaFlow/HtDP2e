929ce8f50ab466e21c393403ffa4743851ff15ab
Alexander <alexander.soto@gmail.com>
2016-02-23 20:09:36 -0500

    Completed Ex14

---
+++ Fixed-Size Data/ex14.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex14) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+#|
+Exercise 14. Define the function cube-volume, which accepts the length of a side of an equilateral cube and computes its volume.
+If you have time, consider defining cube-surface, too.
+|#
+
+(define (cube-volume side)
+  (* side side side))
