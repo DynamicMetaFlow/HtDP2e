05f7bd7a17c0ed7ba6051086b3009aecf3b10fa7
Alexander <alexander.soto@gmail.com>
2016-02-23 22:26:33 -0500

    Completed Ex16

---
+++ Fixed-Size Data/ex16.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex16) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+#|
+Exercise 16. Define the function string-last, which extracts the last 1String from a non-empty string. Don’t worry about empty strings.
+|#
+
+(define (string-last str)
+  (string-ref str (- 1 (string-length str))))
